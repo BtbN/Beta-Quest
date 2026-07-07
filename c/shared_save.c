52f70c48402218335421b2dd853285ce36a2b43f
BtbN <btbn@btbn.de>
2026-07-07 02:21:54 +0200

    DMA/SRAM reads have to be 8 byte aligned

--- c/shared_save.c
+++ c/shared_save.c
@@ -6,12 +6,22 @@
 #define SRAM_SLOT0_OFFSET 0x20
 #define SRAM_SLOT_STRIDE 0x1450
 #define FILE_NAME_BYTES 8
+#define DMA_ALIGN 8
 
 static int32_t restore_active = 0;
 static int32_t restore_file_index = 0;
 
-static inline void read_slot_name(uint8_t* dst, int32_t file_index) {
-    z64_Io(SRAM_PHYS_BASE + SRAM_SLOT0_OFFSET + (file_index * SRAM_SLOT_STRIDE) + offsetof(z64_file_t, file_name), dst, FILE_NAME_BYTES, 0);
+static uint8_t sram_buffer[FILE_NAME_BYTES + DMA_ALIGN] __attribute__((aligned(DMA_ALIGN)));
+
+static void read_slot_name(uint8_t* dst, int32_t file_index) {
+    const uint32_t fname_offset = SRAM_PHYS_BASE + SRAM_SLOT0_OFFSET + (file_index * SRAM_SLOT_STRIDE) + offsetof(z64_file_t, file_name);
+    const uint32_t aligned_offset = fname_offset & ~(DMA_ALIGN - 1);
+    const uint32_t buf_offset = fname_offset - aligned_offset;
+
+    z64_Io(aligned_offset, sram_buffer, sizeof(sram_buffer), 0);
+
+    for (uint32_t i = 0; i < FILE_NAME_BYTES; ++i)
+        dst[i] = sram_buffer[buf_offset + i];
 }
 
 void shared_save_prepare(void) {
